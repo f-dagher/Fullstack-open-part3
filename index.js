861cb12ba87ac17f0da9eb80c8b28ca0df03744c
f-dagher <fdagher95@gmail.com>
2022-10-30 00:49:20 -0400

    3.5 phonebook backend step5

--- index.js
+++ index.js
@@ -3,33 +3,29 @@ const app = express()
 
 let persons = [
     { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456"
       },
       { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
       },
       { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345"
       },
       { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
       }
 ]
-const personsLength = persons.length;
-const date = new Date()
-console.log("persons length", personsLength)
+app.use(express.json())
 
-let info = 
-`<p> Phonebook has info for ${personsLength} people <p> 
- <p> ${date} </p`
+const date = new Date()
 
  //Regular main page
 app.get('/', (request, response) => {
@@ -39,9 +35,16 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
 //info api as a webpage
 app.get('/api/info', (request, response) => {
-    response.send(info)
+  let personsLength = persons.length;
+  let info = 
+    `
+    <p> Phonebook has info for ${personsLength} people <p> 
+    <p> ${date} </p
+    `
+  response.send(info)
 })
 
 //get a single person from phonebook. Respond with error if there isn't an ID.
@@ -59,11 +62,41 @@ app.get('/api/persons/:id', (request, response) => {
 //delete a person from phonebook
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
+  console.log(`deleting name with ${id}`)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
-  
+
+//generate a random ID with a range from maxId to (maxId + 1 000 000 000)
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return Math.floor(Math.random() * 1000000000) + maxId;
+}
+
+//post a new person to the API
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'missing name or number' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  console.log(`added ${person.name}`)
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
