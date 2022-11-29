b8c646f770196ec1b16f2d76c561ccf3058d06d2
f-dagher <fdagher95@gmail.com>
2022-11-29 01:51:48 -0500

    3.16 phonebook database step 4

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const app = express()
 
 const Person = require('./models/person')
 
-
-let persons = [
-    { 
-        id: 1,
-        name: "Arto Hellas", 
-        number: "040-123456"
-      },
-      { 
-        id: 2,
-        name: "Ada Lovelace", 
-        number: "39-44-5323523"
-      },
-      { 
-        id: 3,
-        name: "Dan Abramov", 
-        number: "12-43-234345"
-      },
-      { 
-        id: 4,
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122"
-      }
-]
 app.use(express.json())
 
 app.use(cors())
@@ -80,21 +57,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 //delete a person from phonebook
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(`deleting name with id ${id}`)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
-
-//generate a random ID with a range from maxId to (maxId + 1 000 000 000)
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-  return Math.floor(Math.random() * 1000000000) + maxId;
-}
 
 //post a new person to the API
 app.post('/api/persons', (request, response) => {
@@ -125,6 +94,24 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
@@ -138,4 +125,48 @@ app.listen(PORT, () => {
       error: 'name must be unique' 
     })
   }
+*/
+
+//persons 
+/*
+let persons = [
+    { 
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456"
+      },
+      { 
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
+      },
+      { 
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345"
+      },
+      { 
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
+      }
+]
+
+//old delete
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(`deleting name with id ${id}`)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+//generate a random ID with a range from maxId to (maxId + 1 000 000 000)
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return Math.floor(Math.random() * 1000000000) + maxId;
+}
+
 */
